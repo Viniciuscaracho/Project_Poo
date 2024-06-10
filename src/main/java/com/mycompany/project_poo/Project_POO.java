7f6e3594f9b66aba6b3b31c1733cc70281ae293f
Vinicius Caracho <129306009+Viniciuscaracho@users.noreply.github.com>
2024-06-10 15:30:29 -0300

    fix

---
+++ src/main/java/com/mycompany/project_poo/Project_POO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project_poo;
+
+/**
+ *
+ * @author baby
+ */
+public class Project_POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+       //save plan 
+       
+    }
+}
